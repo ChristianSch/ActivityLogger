fc2294640251b9b5157403abf1905117e96b1a2b
danielnassauer <daniel.nassauer@mni.thm.de>
2014-12-02 08:37:34 +0100

    summary mit beispielen gefüllt

--- activityLogger/www/app/controllers/SummaryCtrl.js
+++ activityLogger/www/app/controllers/SummaryCtrl.js
@@ -1,5 +1,12 @@
 'use strict';
-angular.module('ActivityLogger').controller('SummaryCtrl',
-function ($scope) {
-	
+angular.module('ActivityLogger').controller('SummaryCtrl', function($scope) {
+	$scope.stats = [ {
+		name : "Kilometer gesamt"
+	}, {
+		name : "Zeit gesamt"
+	}, {
+		name : "Kalorienverbrauch gesamt"
+	}, {
+		name : "Durchschnittlicher Kalorienverbrauch"
+	} ];
 })
