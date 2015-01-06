d5563be5b608f9c8d809aca48f99f427f49d2a89
Rene Lutz <rene.lutz@mni.thm.de>
2015-01-06 18:38:25 +0100

    Workout-view and workoutController implemented

--- activityLogger/www/app/controllers/WorkoutCtrl.js
+++ activityLogger/www/app/controllers/WorkoutCtrl.js
@@ -1,4 +1,20 @@
 'use-strict'
-angular.module('ActivityLogger').controller('WorkoutCtrl',
-    function ($scope) {
+angular.module('ActivityLogger')
+    .controller('WorkoutCtrl',
+    function ($scope, $ionicNavBarDelegate, DataService) {
+
+        this.isEditMode = false;
+        if($stateParams.id == 'new') {
+            $ionicNavBarDelegate.setTitle('Neue Activity');
+            this.activity = {};
+            //this.activity.id = localStorage.getItem('nextActivityId');
+        } else {
+            //edit
+            this.isEditMode = true;
+            $ionicNavBarDelegate.setTitle('Activity ' + $stateParams.id);
+            //this.activity = DataService.getActivityById($stateParams.id);
+        }
+        this.save = function() {
+            $ionicNavBarDelegate.back();
+        };
     })
