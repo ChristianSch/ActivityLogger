4596d5e3fcf628248ed64300fd203f15aaca34e1
danielnassauer <daniel.nassauer@mni.thm.de>
2015-01-08 20:29:10 +0100

    added TrackRecord to model

---
+++ activityLogger/www/app/services/model/TrackRecord.js
@@ -0,0 +1,24 @@
+'use strict';
+angular.module('ActivityLogger').factory(
+		'TrackRecord',
+		function() {
+
+			var TrackRecord = function(latitude, longitude, altitude, speed) {
+				this.latitude = latitude;
+				this.logitude = longitude;
+				this.altitude = altitude;
+				this.speed = speed;
+				
+				/**
+				 * Calculates the distance between this and the specified TrackRecord.
+				 * @param track_record TrackRecord
+				 * @return distance
+				 */
+				this.getDistance = function(track_record){
+					//TODO
+					return 1
+				};
+			};
+			return TrackRecord;
+
+		});
