32fdfed6819c71dc2810d743415f2bd3f64f5033
Fosso <elvis.christel.fosso.fotsing@mni.thm.de>
2015-01-18 21:51:07 +0100

    Profil: datepicker ;  app, tab

--- activityLogger/www/app/controllers/TabCtrl.js
+++ activityLogger/www/app/controllers/TabCtrl.js
@@ -4,4 +4,5 @@
 angular.module('ActivityLogger')
     .controller('TabCtrl',
         function($scope, DataService) {
+            $scope.hasUser = DataService.getStatus('user') ? true : false;
         });
