dbd3a8a689d1b4dcb5b868811a761a27b9b75065
Rene Lutz <rene.lutz@mni.thm.de>
2015-01-21 13:48:47 +0100

    Höhenservice implementiert. Klick auf die Karte liefert die aktuelle Höhe

--- activityLogger/www/app/controllers/WorkoutCtrl.js
+++ activityLogger/www/app/controllers/WorkoutCtrl.js
@@ -4,6 +4,10 @@
 angular.module('ActivityLogger')
     .controller('WorkoutCtrl',
     function($stateParams, $scope, $ionicNavBarDelegate, $ionicPopup) {
+        var elevator;
+        var map;
+        var infowindow = new google.maps.InfoWindow();
+
         this.isEditMode = false;
 
         if ($stateParams.id == 'new') {
@@ -39,14 +43,50 @@ angular.module('ActivityLogger')
             });
         };
 
-        this.init = function() {
+        function init() {
             var mapOptions = {
                 center: new google.maps.LatLng(50.587, 8.669),
                 zoom: 10,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
-            var map = new google.maps.Map(document.getElementById("map_canvas"),
-                mapOptions);
-        };
+            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+            elevator = new google.maps.ElevationService();
+            google.maps.event.addListener(map, 'click', getElevation);
+        }
+
+        function getElevation(event) {
+
+            var locations = [];
+
+            // Retrieve the clicked location and push it on the array
+            var clickedLocation = event.latLng;
+            locations.push(clickedLocation);
+
+            // Create a LocationElevationRequest object using the array's one value
+            var positionalRequest = {
+                'locations': locations
+            }
+
+            // Initiate the location request
+            elevator.getElevationForLocations(positionalRequest, function(results, status) {
+                if (status == google.maps.ElevationStatus.OK) {
+
+                    // Retrieve the first result
+                    if (results[0]) {
+
+                        // Open an info window indicating the elevation at the clicked position
+                        infowindow.setContent('The elevation at this point <br>is ' + results[0].elevation + ' meters.');
+                        infowindow.setPosition(clickedLocation);
+                        infowindow.open(map);
+                    } else {
+                        alert('No results found');
+                    }
+                } else {
+                    alert('Elevation service failed due to: ' + status);
+                }
+            });
+        }
+        google.maps.event.addDomListener(window, 'load', init());
     });
