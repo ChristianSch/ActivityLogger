953ade5fe9decc268ca6b085f80937d9227a9c5b
Rene Lutz <rene.lutz@mni.thm.de>
2015-01-21 14:58:58 +0100

    GPS Tracks fürs Testing hinzugefügt

--- activityLogger/www/app/controllers/ActivityListCtrl.js
+++ activityLogger/www/app/controllers/ActivityListCtrl.js
@@ -2,16 +2,32 @@
 
 
 angular.module('ActivityLogger').controller('ActivityListCtrl',
-    function($state, $ionicPopup, $scope, $ionicPopover, Activity, DataService) {
+    function($state, $ionicPopup, $scope, $ionicPopover, Activity, Track, TrackRecord, DataService) {
+
+        var track1 = new Track();
+        track1.addTrackRecord(new TrackRecord(50.5851, 8.6841, 0, 0));
+        track1.addTrackRecord(new TrackRecord(50.5866, 8.6815, 0, 0));
+        track1.addTrackRecord(new TrackRecord(50.5874, 8.6840, 0, 0));
+        track1.addTrackRecord(new TrackRecord(50.5860, 8.6861, 0, 0));
+
+        var track2 = new Track();
+        track2.addTrackRecord(new TrackRecord(50.5851, 8.6841, 0, 0));
+        track2.addTrackRecord(new TrackRecord(50.5837, 8.6847, 0, 0));
+        track2.addTrackRecord(new TrackRecord(50.5828, 8.6802, 0, 0));
+        track2.addTrackRecord(new TrackRecord(50.5839, 8.6783, 0, 0));
+
+        var track3 = new Track();
+        track3.addTrackRecord(new TrackRecord(50.7967, 8.7688, 0, 0));
+        track3.addTrackRecord(new TrackRecord(50.7950, 8.7689, 0, 0));
+        track3.addTrackRecord(new TrackRecord(50.7943, 8.7625, 0, 0));
+        track3.addTrackRecord(new TrackRecord(50.5851, 8.6841, 0, 0));
 
         this.activities = [];
-        this.activities.push(new Activity(1, 'Laufen 100m', 13, 14, [], 'Erster Dummy'));
-        this.activities.push(new Activity(2, 'Laufen 200m', 15, 16, [], 'Zweiter Dummy'));
-        this.activities.push(new Activity(3, 'Laufen 400m', 17, 18, [], 'Dritter Dummy'));
+        this.activities.push(new Activity(1, 'Laufen 100m', 13, 14, track1, 'Erster Dummy'));
+        this.activities.push(new Activity(2, 'Laufen 200m', 15, 16, track2, 'Zweiter Dummy'));
+        this.activities.push(new Activity(3, 'Laufen 400m', 17, 18, track3, 'Dritter Dummy'));
 
         //Todo: Users hinzufügen, wie finde ich aktuellen user?
-        //var userId = DataService.getStatus('userId'); // current userId saved on firebase
-
         //this.activities = DataService.getAllActivitiesByUserID(userId);
 
         this.addActivity = function() {
@@ -23,7 +39,7 @@ angular.module('ActivityLogger').controller('ActivityListCtrl',
 
         //TODO: Don't forget to remove. Only for testing!
         var thisActivities = this.activities;
-        $scope.getActivityByID = function(id) {
+        this.getActivityByID = function(id) {
             for(var i = 0; i < thisActivities.length; i++) {
                 if(thisActivities[i].id == id) {
                     return thisActivities[i];
