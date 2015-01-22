46f02e9874e74ded34fba630ce06f2c148e0fd63
danielnassauer <daniel.nassauer@mni.thm.de>
2015-01-22 19:42:56 +0100

    improved charts

--- activityLogger/www/app/controllers/SummaryCtrl.js
+++ activityLogger/www/app/controllers/SummaryCtrl.js
@@ -1,86 +1,95 @@
 'use strict';
 
-angular.module('ActivityLogger').controller(
-		'SummaryCtrl',
-		function($scope, $compile, $window, Activity, SummaryService) {
+angular.module('ActivityLogger')
+		.controller(
+				'SummaryCtrl',
+				function($scope, $window, Activity, SummaryService) {
 
-			$scope.overallRecords = SummaryService.getOverallRecords();
-			
-			$scope.disciplineRecords = SummaryService.getAllDisciplineRecords();
-			
-			$scope.overallPerformances = SummaryService.getOverallPerformances();
-			
-			//TEST
+					$scope.overallRecords = SummaryService.getOverallRecords();
 
-			this.timesWeekChart = {
-				labels : [ "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" ],
-				datasets : [ {
-					label : "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [ 200, 450, 30, 60, 600, 300, 100 ]
-				} ]
-			};
+					$scope.disciplineRecords = SummaryService
+							.getAllDisciplineRecords();
 
-			this.timesMonthChart = {
-				labels : [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-						"11", "12", "13", "14", "15", "16", "17", "18", "19",
-						"20", "21", "22", "23", "24", "25", "26", "27", "28",
-						"29", "30" ],
-				datasets : [ {
-					label : "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [ 200, 450, 30, 60, 600, 300, 100 ]
-				} ]
-			};
+					$scope.overallPerformances = SummaryService
+							.getOverallPerformances();
 
-			this.timesYearChart = {
-				labels : [ "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul",
-						"Aug", "Sep", "Okt", "Nov", "Dez" ],
-				datasets : [ {
-					label : "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [ 200, 450, 30, 60, 600, 300, 100 ]
-				} ]
-			};
+					/**
+					 * 
+					 */
+					$scope.showTimesWeekChart = function() {
+						var weekdays = [ "MO", "DI", "MI", "DO", "FR", "SA",
+								"SO" ];
+						// rotate weekdays
+						var day = new Date().getDay();
+						weekdays = weekdays.slice(day).concat(
+								weekdays.slice(0, day))
 
-			this.showTimesWeekChart = function() {
-				var ctx = document.getElementById("chart_times_week")
-						.getContext("2d");
-				$window.myLine = new Chart(ctx).Line(this.timesWeekChart, {
-					responsive : true
+						var data = SummaryService.getDurations(7);
+						drawChart(weekdays, data, "chart_times_week");
+					};
+
+					/**
+					 * 
+					 */
+					$scope.showTimesMonthChart = function() {
+						var days = [ "1", "2", "3", "4", "5", "6", "7", "8",
+								"9", "10", "11", "12", "13", "14", "15", "16",
+								"17", "18", "19", "20", "21", "22", "23", "24",
+								"25", "26", "27", "28", "29", "30" ];
+
+						var data = SummaryService.getDurations(30);
+
+						drawChart(days, data, "chart_times_month");
+					};
+
+					/**
+					 * 
+					 */
+					$scope.showTimesYearChart = function() {
+						var days = [ "", "", "", "", "", "", "", "", "", "10",
+								"", "", "", "", "", "", "", "", "", "20", "",
+								"", "", "", "", "", "", "", "", "30", "", "",
+								"", "", "", "", "", "", "", "40", "", "", "",
+								"", "", "", "", "", "", "50", "", "" ];
+						var durations = SummaryService.getDurations(364);
+						var data = [];
+						var i;
+						
+						for (i = 0; i < 364; i+=7) {
+							data.push(durations[i] + durations[i + 1]
+									+ durations[i + 2] + durations[i + 3]
+									+ durations[i + 4] + durations[i + 5]
+									+ durations[i + 6]);
+						}
+						
+						drawChart(days, data, "chart_times_year");
+					};
+
+					/**
+					 * 
+					 */
+					var drawChart = function(labels, data, canvas_id) {
+						var chart = {
+							labels : labels,
+							datasets : [ {
+								label : "",
+								fillColor : "rgba(220,220,220,0.2)",
+								strokeColor : "rgba(220,220,220,1)",
+								pointColor : "rgba(220,220,220,1)",
+								pointStrokeColor : "#fff",
+								pointHighlightFill : "#fff",
+								pointHighlightStroke : "rgba(220,220,220,1)",
+								data : data
+							} ]
+						};
+						var ctx = document.getElementById(canvas_id)
+								.getContext("2d");
+						$window.chart = new Chart(ctx).Line(chart, {
+							showTooltips : false,
+							dataSetStroke : false,
+							pointDot : false,
+							responsive : true
+						});
+					};
+
 				});
-			};
-
-			this.showTimesMonthChart = function() {
-
-				var ctx = document.getElementById("chart_times_month")
-						.getContext("2d");
-				$window.myLine = new Chart(ctx).Line(this.timesMonthChart, {
-					responsive : true
-				});
-			};
-
-			this.showTimesYearChart = function() {
-
-				var ctx = document.getElementById("chart_times_year")
-						.getContext("2d");
-				$window.myLine = new Chart(ctx).Line(this.timesYearChart, {
-					responsive : true
-				});
-			};
-		});
--- activityLogger/www/app/services/SummaryService.js
+++ activityLogger/www/app/services/SummaryService.js
@@ -1,84 +1,158 @@
 'use-strict';
-angular.module('ActivityLogger').factory('SummaryService', function() {
+angular
+		.module('ActivityLogger')
+		.factory(
+				'SummaryService',
+				function(Activity) {
 
-	// PUBLIC FUNCTIONS
+					// PUBLIC FUNCTIONS
 
-	function getOverallRecords() {
-		return [ {
-			name : "Längste Strecke",
-			value : getLongestTrack()
-		}, {
-			name : "Höchste Geschwindigkeit",
-			value : getGreatestSpeed()
-		}, {
-			name : "Größter Höhenunterschied",
-			value : getGreatestHeightDiff()
-		} ];
-	}
+					function getOverallRecords() {
+						return [ {
+							name : "Längste Strecke",
+							value : getLongestTrack()
+						}, {
+							name : "Höchste Geschwindigkeit",
+							value : getGreatestSpeed()
+						}, {
+							name : "Größter Höhenunterschied",
+							value : getGreatestHeightDiff()
+						} ];
+					}
 
-	function getAllDisciplineRecords() {
-		var all_records = [];
-		var disciplines = getAllActivityTypes();
-		var i;
-		for (i in disciplines) {
-			all_records.push({
-				discipline : disciplines[i],
-				records : getRecordsOfDiscipline(disciplines[i])
-			})
-		}
-		return all_records;
-	}
+					function getAllDisciplineRecords() {
+						var all_records = [];
+						var disciplines = getAllActivityTypes();
+						var i;
+						for (i in disciplines) {
+							all_records
+									.push({
+										discipline : disciplines[i],
+										records : getRecordsOfDiscipline(disciplines[i])
+									})
+						}
+						return all_records;
+					}
 
-	function getRecordsOfDiscipline(activity_type) {
-		return [ {
-			name : "Beste Zeit",
-			value : 42
-		}, {
-			name : "Höchste Geschwindigkeit",
-			value : 42
-		}, {
-			name : "Größter Höhenunterschied",
-			value : 42
-		} ];
+					function getRecordsOfDiscipline(activity_type) {
+						return [ {
+							name : "Beste Zeit",
+							value : 42
+						}, {
+							name : "Höchste Geschwindigkeit",
+							value : 42
+						}, {
+							name : "Größter Höhenunterschied",
+							value : 42
+						} ];
 
-	}
+					}
 
-	function getOverallPerformances() {
-		performances = [ {
-			name : "Zurückgelegte Strecke",
-			value : 42
-		}, {
-			name : "Zeit insgesamt",
-			value : 42
-		} ];
-		return performances;
-	}
+					function getOverallPerformances() {
+						performances = [ {
+							name : "Zurückgelegte Strecke",
+							value : 42
+						}, {
+							name : "Zeit insgesamt",
+							value : getOverallTime()
+						} ];
+						return performances;
+					}
 
-	// PRIVATE FUNCTIONS
+					function getActivityDurationPerDay(day) {
+						activities = getRandomActivities();
+						var duration = 0;
+						var i;
+						for (i in activities) {
+							if (isSameDay(activities[i].start_time, day)) {
+								duration += activities[i].duration;
+							}
+						}
+						return duration;
+					}
 
-	function getGreatestSpeed() {
-		return 42;
-	}
+					/**
+					 * Determines the duration of all activities for each day.
+					 * 
+					 * @param {Number}
+					 *            days Number of days.
+					 * @return {Array} Array that contains all durations for
+					 *         each day.
+					 */
+					var getDurations = function(days) {
+						var today = new Date().getTime() / 1000;
+						var durations = [];
+						for (var i = days - 1; i >= 0; i--) {
+							durations.push(getActivityDurationPerDay(today
+									- 86400 * i));
+						}
+						return durations;
+					}
 
-	function getGreatestHeightDiff() {
-		return 42;
-	}
+					// PRIVATE FUNCTIONS
 
-	function getLongestTrack() {
-		return 42;
-	}
+					function getRandomActivities() {
+						var activities = [];
+						var id = 1000;
+						var date = new Date().getTime() / 1000 - 60 * 60 * 10;
+						for (var i = 0; i < 1000; i++) {
+							start = Math.floor(date - Math.random() * 60 * 60
+									* 10);
+							end = Math.floor(start + Math.random() * 60 * 60
+									* 2);
+							date = start;
+							activities.push(new Activity(id--, 'Laufen 100m',
+									start, end, [], ''));
+						}
+						return activities;
+					}
 
-	function getAllActivityTypes() {
-		return [ "Laufen 100m", "Laufen 2KM", "Radfahren 50KM" ];
-	}
+					function isSameDay(timestamp_a, timestamp_b) {
+						date_a = new Date(timestamp_a * 1000).setHours(0, 0, 0,
+								0);
+						date_b = new Date(timestamp_b * 1000).setHours(0, 0, 0,
+								0);
+						if (date_a === date_b) {
+							return true;
+						}
+						return false;
+					}
 
-	var service = {
-		getOverallRecords : getOverallRecords,
-		getRecordsOfDiscipline : getRecordsOfDiscipline,
-		getAllDisciplineRecords : getAllDisciplineRecords,
-		getOverallPerformances : getOverallPerformances
-	};
+					function getGreatestSpeed() {
+						return 42;
+					}
 
-	return service;
+					function getGreatestHeightDiff() {
+						return 42;
+					}
 
-});
+					function getLongestTrack() {
+						return 42;
+					}
+
+					function getOverallTime() {
+						activities = getRandomActivities();
+						var duration = 0;
+						var i;
+						for (i in activities) {
+							duration += activities[i].duration;
+						}
+						return duration;
+					}
+
+					function getAllActivityTypes() {
+						return [ "Laufen 100m", "Laufen 2KM", "Radfahren 50KM" ];
+					}
+
+					var service = {
+						getOverallRecords : getOverallRecords,
+						getRecordsOfDiscipline : getRecordsOfDiscipline,
+						getAllDisciplineRecords : getAllDisciplineRecords,
+						getOverallPerformances : getOverallPerformances,
+						getActivityDurationPerDay : getActivityDurationPerDay,
+						getDurations : getDurations
+					};
+
+					return service;
+
+				});
