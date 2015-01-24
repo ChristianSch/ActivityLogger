73c3a0d321a2c1502e9050abeff2a0e821bc9926
Christian Schulze <cocoanaut@xs-MacBook-Pro-2.local>
2015-01-24 16:48:24 +0100

    implemented map path and distance tracking (fully working)

--- activityLogger/www/app/controllers/ActivityCtrl.js
+++ activityLogger/www/app/controllers/ActivityCtrl.js
@@ -3,8 +3,12 @@
 
 angular.module('ActivityLogger')
     .controller('ActivityCtrl',
-        function($scope, $state, $stateParams, $ionicPopup, DataService, GeoLocationService, Activity) {
+        function($scope, $state, $stateParams, $ionicPopup, DataService,
+            GeoLocationService, Activity) {
+            var thisCtrl = this;
+
             var data = [];
+            var path = []; // for google map
 
             // timer setup
             // init values
@@ -13,6 +17,9 @@ angular.module('ActivityLogger')
             $scope.timerSeconds = 0;
 
             $scope.timerIsOn = false;
+
+            // location values
+            $scope.totalDistance = 0;
 
             // starts (or resumes) timer
             $scope.startTimer = function() {
@@ -40,19 +47,27 @@ angular.module('ActivityLogger')
                         $scope.$digest();
                     }, 1000);
                 }
+
+                thisCtrl.setupGeolocationWatcher();
             };
 
-            // pauses the timer
+            /**
+             * @description Stop the timer interval and geolocation watcher.
+             */
             $scope.stopTimer = function() {
                 if ($scope.timerIsOn === true) {
                     $scope.endStamp = new Date().getTime();
                     $scope.timerIsOn = false;
 
                     clearInterval($scope.timer);
+                    GeoLocationService.stop();
                 }
             };
 
-            // resets all timer vars
+            /**
+             * @description Reset all timer vars to zero and clear the
+             * timer interval.
+             */
             $scope.resetTimer = function() {
                 if ($scope.timerIsOn === true) {
                     $scope.timerIsOn = false;
@@ -67,7 +82,11 @@ angular.module('ActivityLogger')
                 $scope.timerSeconds = 0;
             };
 
-            // shows popup and aborts if affirmative
+            /**
+             * @description Show popup to confirm the cancelation of the
+             * activity. If confirmed all data gets deleted and the
+             * app will return to the main view.
+             */
             $scope.abortActivity = function() {
                 var confirmPopup = $ionicPopup.confirm({
                     title: 'Abort Activity',
@@ -77,11 +96,20 @@ angular.module('ActivityLogger')
                 confirmPopup.then(function(res) {
                     if (res) {
                         $scope.resetTimer();
+                        GeoLocationService.stop();
+
                         $state.go('tab.main');
                     }
                 });
             };
 
+            /**
+             * @description Show popup to confirm the finishing of the activity.
+             * If confirmed, all timers and watchers are
+             * halted and cleaned and the activity will be saved. Otherwise
+             * everything will continue as if nothing happened. The app
+             * then will go to the workout list to show the newly created workout.
+             */
             $scope.finishAcitivity = function() {
                 var confirmPopup = $ionicPopup.confirm({
                     title: 'Finish Activity',
@@ -93,23 +121,61 @@ angular.module('ActivityLogger')
                         $scope.stopTimer();
                         GeoLocationService.stop();
 
-                        var activity = new Activity(1, $stateParams.type, 4, 8, data, $stateParams.comment);
+                        var activity = new Activity(1, $stateParams.type,
+                            4, 8, data, $stateParams.comment);
                         DataService.addActivity(activity);
                         $state.go('tab.workoutlist');
                     }
                 });
             };
 
-            // location setup
-            GeoLocationService.start(function(locationData) {
-                var coords = locationData.coors;
-                // TODO: map
-                
-                data.push(locationData);
-                console.log(locationData);
-            }, function(err) {
-                alert(err);
-            });
+            /**
+             * @description Set up the map and draw the new data as path
+             * 
+             * @param  {Array} data as returned by GeoLocationService
+             */
+            this.refreshMap = function(data) {
+                var options = {
+                    zoom: 14,
+                    center: path[path.length - 1],
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                var map = new google.maps.Map(document.getElementById("map-canvas"),
+                    options);
+
+                var polyline = new google.maps.Polyline({
+                    map: map,
+                    path: path,
+                    strokeColor: '#3876E1',
+                    strokeOpacity: 0.9,
+                    strokeWeight: 10
+                });
+            }
+
+            /**
+             * @description Set up the geo location watcher
+             */
+            this.setupGeolocationWatcher = function() {
+                GeoLocationService.start(function(position) {
+                    var coords = position.coords;
+
+                    path.push(new google.maps.LatLng(
+                        coords.latitude,
+                        coords.longitude));
+
+                    data.push(position);
+
+                    if (data.length > 1) {
+                        $scope.totalDistance = google.maps.geometry.spherical.computeLength(path);
+                        thisCtrl.refreshMap(data);
+                    }
+                }, function(err) {
+                    alert(err);
+                });
+            }
+
+            this.setupGeolocationWatcher();
 
             // start the timer on initialization
             $scope.startTimer();
