f878dd3c157119470e1678214ec7c6a39eb89d7a
Rene Lutz <rene.lutz@mni.thm.de>
2015-01-26 00:38:20 +0100

    Added userId to Activity

--- activityLogger/www/app/services/model/Activity.js
+++ activityLogger/www/app/services/model/Activity.js
@@ -4,8 +4,9 @@
 angular.module('ActivityLogger').factory('Activity',
     function() {
         var Activity = function(id, type, start_time, end_time, track_data,
-            comment, distance) {
+            comment, distance, userId) {
             this.id = id;
+            this.userId = userId;
             this.type = type;
             this.start_time = start_time;
             this.end_time = end_time;
