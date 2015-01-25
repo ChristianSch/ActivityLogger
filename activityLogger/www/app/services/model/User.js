f9331ccccff1f1b1e3b3cc1919657cc7ff9ae556
danielnassauer <daniel.nassauer@mni.thm.de>
2015-01-25 22:56:22 +0100

    Änderung am Model war nicht abgesprochen. UsersName entfernt. Benutzername ist ID

--- activityLogger/www/app/services/model/User.js
+++ activityLogger/www/app/services/model/User.js
@@ -3,12 +3,11 @@
 
 angular.module('ActivityLogger').factory('User',
     function() {
-        var User = function(id, firstname, surname,usersName, gender, birthday,
+        var User = function(id, firstname, surname, gender, birthday,
             weight, size) {
             this.id = id;
             this.firstname = firstname;
             this.surname = surname;
-            this.usersName=usersName;
             this.gender = gender;
             this.birthday = birthday;
             this.weight = weight;
