7eee6909497a956d676a0b21fe3d0e02c791cd10
Rene Lutz <rene.lutz@mni.thm.de>
2015-01-25 15:01:27 +0100

    Added distance to Activity

--- activityLogger/www/app/services/model/Activity.js
+++ activityLogger/www/app/services/model/Activity.js
@@ -4,13 +4,14 @@
 angular.module('ActivityLogger').factory('Activity',
     function() {
         var Activity = function(id, type, start_time, end_time, track_data,
-            comment) {
+            comment, distance) {
             this.id = id;
             this.type = type;
             this.start_time = start_time;
             this.end_time = end_time;
             this.track_data = track_data;
             this.comment = comment;
+            this.distance = distance;
             this.duration = this.end_time - this.start_time;
         };
 
