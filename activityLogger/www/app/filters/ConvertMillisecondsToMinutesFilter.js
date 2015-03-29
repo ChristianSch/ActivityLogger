9143330bff27c38704a38f8d38d2d5cecab01437
Rene Lutz <rene.lutz@mni.thm.de>
2015-03-29 13:24:55 +0200

    Edited normalization in Filter (second number is now displayed if a value is less than 10)

--- activityLogger/www/app/filters/ConvertMillisecondsToMinutesFilter.js
+++ activityLogger/www/app/filters/ConvertMillisecondsToMinutesFilter.js
@@ -18,7 +18,17 @@
             return function(milliseconds) {
                 var minutes = (milliseconds / (1000 * 60));
                 var seconds = (milliseconds / 1000) % 60;
-                return $filter('number')(minutes , 0) + ":" + $filter('number')(seconds, 2);
+
+                var minuteString = $filter('number')(minutes, 0);
+                var secondString = $filter('number')(seconds, 2);
+                if(minutes < 10) {
+                    minuteString = "0" + minuteString;
+                }
+                if(seconds < 10) {
+                    secondString = "0" + secondString;
+                }
+
+                return minuteString + ":" + secondString;
 
             };
         });
--- activityLogger/www/app/filters/NormalizeDateFilter.js
+++ activityLogger/www/app/filters/NormalizeDateFilter.js
@@ -18,9 +18,21 @@
             return function(timestamp) {
                 var dateString;
                 var date = new Date(timestamp);
+                var hourString = date.getHours();
+                var minuteString = date.getMinutes();
+                var secondString = date.getSeconds();
 
+                if(date.getHours()< 10) {
+                    hourString = "0" + hourString;
+                }
+                if(date.getMinutes() < 10) {
+                    minuteString = "0" + minuteString;
+                }
+                if(date.getSeconds() < 10) {
+                    secondString = "0" + secondString;
+                }
                 dateString = date.getDate() + "." + (date.getMonth()+1) + "." + date.getFullYear() + " " +
-                             date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+                             hourString + ":" + minuteString + ":" + secondString;
 
                 return dateString;
             };
